1c4a2226a030a31458c0f226f18314a1594568d7
202300324 <202300324@estudantes.ips.pt>
2024-03-15 10:29:32 +0000

    Nivel 2 incompleto

--- WordGuessingGame.java
+++ WordGuessingGame.java
@@ -1,44 +1,72 @@
+import java.util.Arrays;
 
 public class WordGuessingGame
 {
     private String hiddenWord;
     private String guessedWord;
     private int numberOfTries;
-    
+    public InputReader reader;
+    private String letter;
     
     public WordGuessingGame (){
         this.hiddenWord = "abc";
         this.guessedWord = "___";
         this.numberOfTries = 0;
+        this.reader = reader;
     }
     
-    public String getHiddenWord(){
+    private String getHiddenWord(){
         return hiddenWord;
     }
     
-    public void setHiddenWord(String hiddenWord){
+    private void setHiddenWord(String hiddenWord){
         this.hiddenWord = hiddenWord;
     }
     
-    public String getGuessedWord(){
+    private String getGuessedWord(){
         return guessedWord;
     }
     
-    public void setGuessedWord(String guessedWord){
+    private void setGuessedWord(String guessedWord){
         this.guessedWord = guessedWord;
     }
     
-    public int getNumberOfTries(){
+    private int getNumberOfTries(){
         return numberOfTries;
     }
     
-    public void setNumberOfTries(int numberOfTries){
+    private void setNumberOfTries(int numberOfTries){
         this.numberOfTries = numberOfTries;
     }
     
-    public String showGuessedWord(){
+    private String showGuessedWord(){
         return guessedWord;
     }
     
     
+    public void play(){
+        
+    }
+    
+    private String showWelcome(){
+        String str = "Bem vindo :)" ;
+        return str;
+    }
+    
+    public char[] guess(String letter){
+        this.letter = letter;
+        char[] hiddenChars = hiddenWord.toCharArray();
+        char[] guessedChars = guessedWord.toCharArray();
+        int num = 0;
+        int a = 0;
+        for (String x: hiddenChars.toString()){
+            if (x.equals(letter)){
+                guessedChars[num] = hiddenChars[i];
+            }
+            System.out.println("ui");
+        }
+        num++;
+        return guessedChars; 
+      
+    }
 }
